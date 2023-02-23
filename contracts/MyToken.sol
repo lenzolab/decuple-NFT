d65e41f64c85b8d08481947ee013b27073392861
lenzolab <info@lenzolab.com>
2023-02-23 06:01:15 +0330

    init repo

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,29 +2,71 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, Pausable, Ownable {
-    constructor() ERC721("MyToken", "MTK") {}
+contract Decuple is ERC721, Pausable, Ownable {
+    constructor() ERC721("Decuple NFT", "CDPN") {}
 
-    function pause() public onlyOwner {
-        _pause();
-    }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
-    }
+    //╔═════════════════════════════════════════════════════════════════════════════════╗
+    //║                                                                                 ║
+    //║                                     Mint                                        ║
+    //║                                                                                 ║
+    //╚═════════════════════════════════════════════════════════════════════════════════╝
+    //    
+        function safeMint(address to, uint256 tokenId) public onlyOwner {
+            _safeMint(to, tokenId);
+        }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+        function mint(uint256 tokenId) public {
+            address sndr = msg.sender;
+
+            _safeMint(to, tokenId);
+
+        }
+    //  =============== END of Mint
+
+    //╔═════════════════════════════════════════════════════════════════════════════════╗
+    //║                                                                                 ║
+    //║                                 Payments                                        ║
+    //║                                                                                 ║
+    //╚═════════════════════════════════════════════════════════════════════════════════╝
+    //    
+
+        IERC busd, usdc, usdt, fourth, fifth, sixth;
+
+
+        
+    //  =============== END of Mint
+
+
+
+    //╔═════════════════════════════════════════════════════════════════════════════════╗
+    //║                                                                                 ║
+    //║                                   Administrative                                ║
+    //║                                                                                 ║
+    //╚═════════════════════════════════════════════════════════════════════════════════╝
+    //
+        function pause() public onlyOwner {
+            _pause();
+        }
+
+        function unpause() public onlyOwner {
+            _unpause();
+        }
+
+
+        function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+            internal
+            whenNotPaused
+            //override
+        {
+            super._beforeTokenTransfer(from, to, tokenId, 1);
+        }
+
+    //  =============== END of Administration
+
 }
